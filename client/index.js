045b973d985831b4bf979691b9eefac068675b56
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-04-04 23:02:39 +0500

    adding SongDetail route

--- client/index.js
+++ client/index.js
@@ -8,6 +8,7 @@ import { Route, Router, hashHistory, IndexRoute } from "react-router";
 import App from "./components/App.js";
 import SongList from "./components/SongList";
 import SongCreate from "./components/SongCreate";
+import SongDetail from "./components/SongDetail";
 
 import "./style/style.css";
 
@@ -20,6 +21,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
