e9bff0d02613dd54dcc9b5cbfabeac0190dbc40f
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-04-05 15:17:22 +0500

    adding fetch song query adding query params & pass down to query as a params

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+
+import fetchSong from "../queries/fetchSong";
 
 class SongDetail extends Component {
   render() {
+    console.log("Songs Detail props is: ", this.props);
     return (
       <div>
         <h3>Song Detail</h3>
@@ -10,4 +14,10 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  // this is used to grab query params from URL & pass down
+  // to GraphQL query as params id
+  options: props => {
+    return { variables: { id: props.params.id } };
+  }
+})(SongDetail);
