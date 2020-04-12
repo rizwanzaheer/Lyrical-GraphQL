b968d64b75531312ac634d681c7b5151bc18f67f
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-04-12 23:29:05 +0500

    adding likes mutation & show likes

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 class LyricList extends Component {
   onLike(id) {
     console.log("onLike id is: ", id);
+    this.props.mutate({
+      variables: { id },
+    });
   }
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className=" collection-item">
           {content}
           <i className="material-icons" onClick={() => this.onLike(id)}>
             thumb_up
           </i>
+          {likes}
         </li>
       );
     });
@@ -25,4 +31,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
